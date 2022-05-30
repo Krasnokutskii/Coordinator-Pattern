5190f792cfc19ee542f42eb10e9193ff46b3db8e
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2022-05-30 18:21:35 +0300

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Yaroslav Krasnokutskiy on 30.5.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
